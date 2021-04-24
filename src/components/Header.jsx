7a3d6a406528f4681c660ad4e9ab5f7f3f0c914d
Ahmed-Maher164 <ahmed164maher@gmail.com>
2021-04-24 15:00:29 +0200

    add hooks userPosts , get userPosts Function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,18 @@
 import {useState} from "react"
 import { usePosts } from "./hooks/usePosts";
 
-function getUserPosts(){
-    // useState
-}
 
-export function Header({params}){
+
+export function Header({ user_id, getUserPosts }){
     const { getUserPosts } = usePosts([
-        { id: 1, title: "first post", completed: true },
-        { id: 2, title: "second post", completed: true },
+        { id: 1, title: "first post", body: " body 1 " , user_id:1 },
+        { id: 2, title: "second post", body: " body 2 " , user_id:1 },
       ]);  
     return(
         <div className="mb-2">
         <label htmlFor="user">userID</label>
         <input type="text" id="user"/>
-        <button onClick={getUserPosts}>
+        <button onClick={() => getUserPosts(user_id)}>
             get posts
         </button>
         
