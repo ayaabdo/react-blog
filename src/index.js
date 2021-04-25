5db66de05aa1a6e34e28bafe0dcf8facfe12d754
Ahmed-Maher164 <ahmed164maher@gmail.com>
2021-04-25 19:25:53 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Auth} from './components/Auth';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth/>
   </React.StrictMode>,
   document.getElementById('root')
 );
